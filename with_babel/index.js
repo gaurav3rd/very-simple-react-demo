76f8cc407300653ff588430d8f9cb1308c797844
Gaurav Jaiswal <gaurav.jaiswal@khalti.com>
2023-11-16 13:28:37 +0545

    use of babel

---
+++ with_babel/index.js
@@ -0,0 +1,20 @@
+// Corrected the function and moved code outside
+function createButton() {
+    const [isLiked, setLike] = React.useState(false);
+
+    if (isLiked) {
+        return <h1>you liked it!</h1>;
+    } else {
+        // Create an element of type button to like the post and return it
+        return <button onClick={()=>setLike(true)}>Like</button>
+    }
+}
+
+// Grab the root element where the new element should be injected
+const rootElement = document.getElementById("root");
+
+// Create a root element using rootElement
+const root = ReactDOM.createRoot(rootElement);
+
+// Call the createButton function and render the result
+root.render(React.createElement(createButton));
